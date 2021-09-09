f102df6e9591a0967df7123226c9bd25bd9dbe37
henken <henken@gmail.com>
2021-09-09 14:46:55 -0400

    henken.io mirror

--- js/henken.js
+++ js/henken.js
@@ -1,3 +1,15 @@
+var url = window.location.href;
+var host = window.location.host;
+var path = window.location.pathname;
+var query = window.location.search;
+var hash = window.location.hash;
+
+console.log('url: ' + url);
+console.log('host: ' + host);
+console.log('path: ' + path);
+console.log('query: ' + query);
+console.log('hash: ' + hash);
+
 // favicon
 const favicon = document.querySelector('link[rel="shortcut icon"]');
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
@@ -5,3 +17,15 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
 } else {
 	favicon.setAttribute('href','./img/favicon/favicon-light.svg');
 }
+
+// copyright
+var d = new Date();
+var year = d.getFullYear();
+var copyright = document.querySelector('footer > span');
+copyright.innerHTML = '&copy; ' + year;
+
+// section fadein
+var section = document.querySelector('section');
+setTimeout(function(){
+	section.classList.add('active');
+},4200);
